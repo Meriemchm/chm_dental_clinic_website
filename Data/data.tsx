cfb102815fee25953103af5c8468fe00f8bd6297
Meriemchm <chamimeriem2302@gmail.com>
2025-10-10 15:37:22 +0100

    create data component

---
+++ Data/data.tsx
@@ -0,0 +1,22 @@
+export const NavbarItems = [
+  {
+    id: 1,
+    name: "home",
+    link: "/",
+  },
+  {
+    id: 2,
+    name: "about us",
+    link: "/about-us",
+  },
+  {
+    id: 3,
+    name: "services",
+    link: "/services",
+  },
+  {
+    id: 3,
+    name: "contact us",
+    link: "/contact-us",
+  },
+];
