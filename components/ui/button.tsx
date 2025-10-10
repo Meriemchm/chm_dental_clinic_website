c1215dc0b3cfc945d45242f59ed7d98a3b2a7f3a
Meriemchm <chamimeriem2302@gmail.com>
2025-10-10 15:36:53 +0100

    create customzable button

---
+++ components/ui/button.tsx
@@ -0,0 +1,25 @@
+import { cn } from "@/lib/utils";
+import * as React from "react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className = "", children, disabled, type = "button", ...props }, ref) => {
+    return (
+      <button
+        className={cn(
+          "bg-primary px-4 py-2 text-white rounded-full hover:bg-primary/90 transition text-sm md:text-base",
+          className
+        )}
+        disabled={disabled}
+        type={type}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+export default Button;
