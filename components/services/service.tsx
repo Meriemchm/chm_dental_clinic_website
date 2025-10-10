4e2a5cd2f1b538a0bacbacd2f434570e567a49ec
Meriemchm <chamimeriem2302@gmail.com>
2025-10-10 21:56:31 +0100

    create service section

---
+++ components/services/service.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { ServicesData } from "@/Data/data";
+import Image from "next/image";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+
+export const Service = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 items-center justify-between">
+      {ServicesData.map((item, index) => (
+        <div
+          key={index}
+          className={cn(
+            " flex flex-col items-start text-start gap-6 p-6 rounded-xl ",
+            index === 1 ? "border-b-2 border-primary" : "border"
+          )}
+        >
+          <div className="bg-primary rounded-full p-4">
+            <Image src={item.icon} alt={item.title} height={50} width={50} />
+          </div>
+          <h3 className="text-2xl text-black font-semibold mb-2">
+            {item.title}
+          </h3>
+          <p className="text-neutral-500 text-sm md:text-base">
+            {item.description}
+          </p>
+          <Link
+            href="/"
+            className={cn(
+              "group flex items-center gap-2  font-semibold",
+              index === 1 ? "text-primary" : "text-black"
+            )}
+          >
+            Learn More{" "}
+            <Image
+              src="/Icons/arrow.svg"
+              alt="arrow"
+              height={20}
+              width={20}
+              className={cn(
+                "group-hover:translate-x-2 transform duration-200",
+                index === 1 ? "text-primary" : "filter brightness-0"
+              )}
+            />{" "}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
---
+++ components/services/services.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Button from "../ui/button";
+
+import { Service } from "./service";
+
+const Services = () => {
+  return (
+    <div className="flex flex-col gap-16 my-20">
+      <div className="flex w-full items-center justify-between flex-wrap gap-4">
+        <div className="flex flex-col gap-4">
+          <p className="uppercase text-black">Our Services</p>
+
+          <h2 className="md:text-5xl text-3xl font-bold text-primary max-w-md">
+            What We Offer{" "}
+            <span className="text-black ">For your best smile</span>
+          </h2>
+        </div>
+
+        <Button className="text-sm md:text-base">Explore All Services</Button>
+      </div>
+      {/*services*/}
+      <Service />;
+    </div>
+  );
+};
+
+export default Services;
