4532b5e2557aab269fa1a41f34518486e0f1143a
Meriemchm <chamimeriem2302@gmail.com>
2025-10-10 22:34:30 +0100

    create contact us page

---
+++ app/(routes)/contact-us/page.tsx
@@ -0,0 +1,14 @@
+
+import HearderTitle from "@/components/ui/hearderTitleSection";
+import React from "react";
+
+const ContactUsPage = () => {
+  return (
+    <div>
+      <HearderTitle title="Contact Us" url="Contact Us" />
+    
+    </div>
+  );
+};
+
+export default ContactUsPage;
